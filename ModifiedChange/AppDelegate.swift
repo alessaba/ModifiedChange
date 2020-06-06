5348c3dd6784dbe8ed481bc5acb5d7442f889b82
alessaba <alessandrosaba023@gmail.com>
2020-06-06 18:25:00 +0200

    Initial Commit

---
+++ ModifiedChange/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ModifiedChange
+//
+//  Created by Alessandro Saba on 06/06/2020.
+//  Copyright © 2020 Alessandro Saba. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ ModifiedChange/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ModifiedChange
+//
+//  Created by Alessandro Saba on 06/06/2020.
+//  Copyright © 2020 Alessandro Saba. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
